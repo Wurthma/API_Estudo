5a539de5f44f7bf4cdc712a3cce80fbd056bbf28
George Henrique Wurthmann <gemakai@gmail.com>
2021-02-06 11:47:16 -0300

    Preparando entidades e VOs

---
+++ Store.Domain/Context/Enums/EDeliveryStatus.cs
@@ -0,0 +1,10 @@
+namespace Store.Domain.Context.Enums
+{
+    public enum EDeliveryStatus
+    {
+        Waiting = 1,
+        Shipped = 2,
+        Delivered = 3,
+        Canceled = 4
+    }
+}
---
+++ Store.Domain/Context/Enums/EOrderStatus.cs
@@ -0,0 +1,10 @@
+namespace Store.Domain.Context.Enums
+{
+    public enum EOrderStatus
+    {
+        Created = 1,
+        Paid = 2,
+        Shipped = 3,
+        Canceled = 4
+    }
+}
