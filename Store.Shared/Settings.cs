daad0b6efe562819b3288f53a7768be7cb1f191a
George Henrique Wurthmann <gemakai@gmail.com>
2021-02-08 01:28:04 -0300

    Procedures, dapper, queries e queries results

---
+++ Store.Shared/Settings.cs
@@ -0,0 +1,7 @@
+namespace Store.Shared
+{
+    public static class Settings
+    {
+        public static string ConnectionString = @"Server=.\sqlexpress;Database=Store;Trusted_Connection=True;";
+    }
+}
